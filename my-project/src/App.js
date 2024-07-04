e0f38d7db9e4165b54afea81e601a2ee36dbf3e9
Bew60000 <anw.nattapon.w@gmail.com>
2024-07-04 10:25:36 +0700

    Edit toLocaleString() in 182

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -177,7 +177,10 @@ function App() {
                   <Table.Cell>{val.course_Name}</Table.Cell>
                   <Table.Cell>{val.category_Name}</Table.Cell>
                   <Table.Cell>{val.totalStudents.toLocaleString()} คน</Table.Cell>
-                  <Table.Cell>{val.totalCert} ใบ</Table.Cell>
+                  <Table.Cell>
+                    {/* {val.totalCert.toLocaleString() } ใบ */}
+                    {val.totalCert !== undefined && val.totalCert !== null ? val.totalCert.toLocaleString() : '0'} ใบ
+                  </Table.Cell>
                 </Table.Row>
               )}
             </Table.Body>
